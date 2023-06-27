ea75e933f1714047def2c26cb111b8ff54be0dde
OwerFrank <olopeza@unsa.edu.pe>
2023-06-27 15:04:04 -0500

    Creamos la carpeta MEDIA para poder guardar dentra de ella todas las imagenes, modificando el settings y urls

--- lugares_turisticos/destinos/models.py
+++ lugares_turisticos/destinos/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class DestinosTuristicos(models.Model):
     nombreCiudad = models.CharField(max_length=100)
     descripcionCiudad = models.TextField()
-    imagenCiudad = models.ImageField(upload_to='destinos/')
+    imagenCiudad = models.ImageField(upload_to='imagenes/')
     precioTour = models.DecimalField(max_digits=8, decimal_places=2)
     ofertaTour = models.BooleanField(default=False)
 
