9ba75378787b310ad74109477de82baa79199366
OwerFrank <olopeza@unsa.edu.pe>
2023-06-27 22:10:20 -0500

    creamos el views.py para definir las vistas de registro, inicio de sesión y cierre de sesión

--- lugares_turisticos/cuentas/views.py
+++ lugares_turisticos/cuentas/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import RegistroForm, InicioSesionForm
 
 # Create your views here.
+
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inicio_sesion')
+    else:
+        form = RegistroForm()
+    return render(request, 'registro.html', {'form': form})
+
+def inicio_sesion(request):
+    if request.method == 'POST':
+        form = InicioSesionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('inicio')
+    else:
+        form = InicioSesionForm()
+    return render(request, 'inicio_sesion.html', {'form': form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('inicio')
