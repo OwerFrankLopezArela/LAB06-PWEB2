7bb619593f03a31fec9f2e715ba4b6970bd645f9
OwerFrank <olopeza@unsa.edu.pe>
2023-06-28 02:37:00 -0500

    Creamos las vistas que se hace al dar en ver a nuestros lugares turisticos, para ello añadimos una nueva url y creamos un nuevo html

--- lugares_turisticos/destinos/urls.py
+++ lugares_turisticos/destinos/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.destinos_turisticos, name='destinos_turisticos')
+    path('', views.destinos_turisticos, name='destinos_turisticos'),
+    path('destino/<int:destino_id>/', views.detalle_destino, name='detalle_destino'),
 ]
